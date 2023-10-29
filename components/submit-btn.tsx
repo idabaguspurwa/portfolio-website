27a94d88623f0edea25faf948328c0ba5902f947
Ida Bagus Gede Purwa Manik A <ida.bagus862@gmail.com>
2023-10-29 17:21:09 +0700

    third commit

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { FaPaperPlane } from "react-icons/fa";
-import { useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
-  const { pending } = useFormStatus();
+  const pending = false; // Assuming a default value for pending
   return (
     <button
       type="submit"
